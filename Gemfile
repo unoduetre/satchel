5c43af1b977459550070b24ad34ebdd92a2deee5
Mateusz Grotek <mat@ideapiece.com>
2023-11-09 16:36:07 +0000

    Initial application setup

--- Gemfile
+++ Gemfile
@@ -1,35 +1,49 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-ruby "3.2.2"
+source 'https://rubygems.org'
 
-gem "rails", "~> 7.1.1"
-gem "sprockets-rails"
-gem "pg", "~> 1.1"
-gem "puma", ">= 5.0"
-gem "importmap-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
-gem "dartsass-rails"
-gem "jbuilder"
-gem "redis", ">= 4.0.1"
+ruby '3.2.2'
 
-# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
-# gem "kredis"
+gem 'dotenv-rails', groups: %i[development test]
 
-# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
-
-gem "tzinfo-data", platforms: %i[ windows jruby ]
-
-gem "bootsnap", require: false
-
-# Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem 'bootsnap', require: false
+gem 'dartsass-rails'
+gem 'importmap-rails'
+gem 'jbuilder'
+gem 'pg', '~> 1.1'
+gem 'puma', '>= 5.0'
+gem 'rails', '~> 7.1.1'
+gem 'redis', '>= 4.0.1'
+gem 'sprockets-rails'
+gem 'stimulus-rails'
+gem 'turbo-rails'
+gem 'tzinfo-data', platforms: %i[windows jruby]
 
 group :development, :test do
+  gem 'brakeman'
+  gem 'bundler-audit'
+  gem 'factory_bot_rails', '~> 6.2'
+  gem 'faker', '~> 3.2'
+  gem 'pry-byebug'
+  gem 'pry-doc'
+  gem 'pry-inline'
+  gem 'pry-rails'
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
 end
 
 group :development do
-  gem "web-console"
-  gem "rack-mini-profiler"
+  gem 'memory_profiler'
+  gem 'rack-mini-profiler', require: false
+  gem 'rubocop-factory_bot', require: false
+  gem 'rubocop-faker', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
+  gem 'stackprof'
+  gem 'web-console'
+end
+
+group :test do
+  gem 'rspec-rails', '~> 6.0'
+  gem 'shoulda-matchers', '~> 5.3'
 end
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,19 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails"
+require_relative 'boot'
+
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -19,6 +21,7 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module Satchel
+  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -26,7 +29,7 @@ module Satchel
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -37,6 +40,14 @@ module Satchel
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.system_tests false
+      g.helper false
+      g.helper_specs false
+      g.view_specs false
+      g.routing_specs false
+    end
   end
 end
